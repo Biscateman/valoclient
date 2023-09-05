7ad555dc177c899ce5baebb13819d0db82afde78
VamshiKrishna <vamshikrishna9246@gmail.com>
2023-09-05 17:54:28 +0530

    your message for this commit

--- src/components/Newagent/Newagent.js
+++ src/components/Newagent/Newagent.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Newagent.css'
 
 const Newagent = () => {
 
-    const handleSubmit = () => {
+    const [name, setName] = useState('')
+    const [imgUrl, setImgUrl] = useState('')
+    const [role, setRole] = useState('')
+    const [country, setCountry] = useState('')
+
+    const options = ['Duelist', 'Initiator', 'Controller', 'Sentinel']
+
+    const handleChange = (e) => {
+        setRole(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
         const url = 'http://localhost:3000/newagent';
 
+        e.preventDefault()
+
+        console.log('hey')
+
+        console.log(name, imgUrl, role, country)
+
         const postData = {
-            key1: 'value1',
-            key2: 'value2'
+            name: name,
+            imgUrl: imgUrl,
+            role: role,
+            country: country
         };
 
         fetch(url, {
@@ -32,25 +51,26 @@ const Newagent = () => {
             <form onSubmit={handleSubmit}>
                 <div className='row'>
                     <label htmlFor='name'>Name:</label>
-                    <input name='name' id='name' required={true}  ></input>
+                    <input name='name' id='name' onChange={(e) => setName(e.target.value)} required={true} value={name} ></input>
                 </div>
                 <div className='row'>
                     <label htmlFor='imgUrl' >ImageUrl:</label>
-                    <input name='imgUrl' id='imgUrl' required={true} ></input>
+                    <input name='imgUrl' id='imgUrl' onChange={(e) => setImgUrl(e.target.value)} required={true} value={imgUrl} ></input>
                 </div>
                 <div className='row'>
                     <label htmlFor='role' >Role:</label>
-                    <select name='role' id='role' required={true} >
-                        <option>Select the role</option>
-                        <option>Duelist</option>
-                        <option>Initiator</option>
-                        <option>Controller</option>
-                        <option>Sentinel</option>
+                    <select name='role' id='role' value={role} onChange={handleChange} required={true} >
+                        <option>Select</option>
+                        {options.map((value) => (
+                            <option value={value} key={value}>
+                                {value}
+                            </option>
+                        ))}
                     </select>
                 </div>
                 <div className='row'>
                     <label htmlFor='country'>Country:</label>
-                    <input name='country' id='country' required={true} ></input>
+                    <input name='country' onChange={(e) => setCountry(e.target.value)} id='country' value={country} required={true} ></input>
                 </div>
                 <button className='btn' type='submit'>Add</button>
             </form>
